2d0efc55b50838e31db435a27943504aa4ce2a2f
Fernando-Arg <lf97ch@gmail.com>
2021-03-05 08:42:28 -0500

    btn scroll up, efecto typing footer, btd download

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
 const desplazar = document.querySelector('.navbar')
-
+const regresar = document.querySelector('.scroll-up')
 window.onscroll= () => {
     let scroll = document.documentElement.scrollTop || document.body.scrollTop;
     if (scroll > 20 ){
-        desplazar.classList.add('navbar-scroll')    
+        desplazar.classList.add('navbar-scroll')
     }
     else{
         desplazar.classList.remove('navbar-scroll')
     }
+        if (scroll >500){
+            regresar.classList.add('active')    
+        }
+        else{
+            regresar.classList.remove('active')    
+        }
 }
+
+/*SCRIPT REGRESAR*/
+
+regresar.addEventListener('click', () =>{
+    const htmlp = document.documentElement.scrollTop
+    if(htmlp > 0){
+        window.scrollTo(0, 0)
+    }
+    
+})
 
 /*SCRIPT MENU*/
 const boton = document.querySelector('.menu-btn i')
@@ -19,8 +35,29 @@ boton.addEventListener('click', () => {
     boton.classList.toggle('active')
 })
 
-
+/*SCRIP BTN INICIO*/
 /*SCROLLREVEAL*/
-ScrollReveal().reveal('.about',{delay: 500 });
-ScrollReveal().reveal('.services',{delay: 500 });
-ScrollReveal().reveal('.skills',{delay: 500 });
+ScrollReveal().reveal('.about',{delay: 600 });
+ScrollReveal().reveal('.services',{delay: 600 });
+ScrollReveal().reveal('.skills',{delay: 600 });
+
+
+/*ESCRITURA DEVELOPER JR*/
+const typed = new Typed('.write', {
+    strings: ['Developer Jr.'],
+    typeSpeed:100,
+    backSpeed:60,
+    backDelay:1000,
+    showCursor:false,
+    loop: true
+} );
+
+
+const typed2 = new Typed('.write-2', {
+    strings: ['Developer Jr.'],
+    typeSpeed:100,
+    backSpeed:60,
+    backDelay:1000,
+    showCursor:false,
+    loop: true
+} );
